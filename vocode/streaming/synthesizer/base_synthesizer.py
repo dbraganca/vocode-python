e1d115781c83f461f0272084377cffcc4d561456
Diogo Braganca <diogobraganca93@gmail.com>
2023-09-19 20:50:26 +0100

    simpler and less risky FILLER_PHRASES

--- vocode/streaming/synthesizer/base_synthesizer.py
+++ vocode/streaming/synthesizer/base_synthesizer.py
@@ -32,12 +32,13 @@ from vocode.streaming.models.synthesizer import SynthesizerConfig
 FILLER_PHRASES = [
     BaseMessage(text="Um - "),
     BaseMessage(text="Uh - "),
-    BaseMessage(text="Uh-huh - "),
-    BaseMessage(text="Mm-hmm - "),
-    BaseMessage(text="Hmm - "),
-    BaseMessage(text="Okay - "),
-    BaseMessage(text="Right - "),
-    BaseMessage(text="Let me see - "),
+    # BaseMessage(text="Uh-huh - "),
+    # BaseMessage(text="Mmhmm - "),
+    # BaseMessage(text="Hmm - "),
+    BaseMessage(text="Yeah - "),
+    # BaseMessage(text="Okay - "),
+    # BaseMessage(text="Right - "),
+    # BaseMessage(text="Let me see - "),
 ]
 FILLER_AUDIO_PATH = os.path.join(os.path.dirname(__file__), "filler_audio")
 TYPING_NOISE_PATH = "%s/typing-noise.wav" % FILLER_AUDIO_PATH
